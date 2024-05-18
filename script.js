848b1944a655995f6575754d4ff7d97693058588
MadhavBorkar <157888465+MadhavBorkar@users.noreply.github.com>
2024-05-18 23:02:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function updateTimer() {
+    const timerElement = document.getElementById('timer');
+    const now = new Date();
+
+    const formattedDate = now.toLocaleDateString();
+    const formattedTime = now.toLocaleTimeString();
+
+    timerElement.textContent = `${formattedDate} ${formattedTime}`;
+}
+setInterval(updateTimer, 1000);
+updateTimer(); 
